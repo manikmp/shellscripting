35e3da2fe137cd225b76d799fcbe9f14c20c0ef8
manikmp <manivajrala1992@gmail.com>
2022-07-07 17:03:23 +0530

    helloworld

---
+++ 02-print.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo Hello World
+
+## color codes
+# Red       31
+# green     32
+# Yellow    33
+# Blue      34
+# Magenta   35
+# Cyan      36
+
+# Syntax : echo -e "\e[31mHello\[0m"
