ad231b2418f26256e77865e14a1a2d966a87953d
manikmp <manivajrala1992@gmail.com>
2022-07-08 16:23:16 +0530

    variables , Arrays

--- 05-Quotes.sh
+++ 05-Quotes.sh
@@ -4,7 +4,8 @@ echo 'Apple cost is $100'
 echo "Apple cost is $200"
 echo "Apple cost is \$300"
 
-Training = Devops
+Training=Devops
 echo 'training = ${Training}'
-echo "training = $ Training"
+echo "training = ${Training}"
 echo "training = \${Training}"
+
