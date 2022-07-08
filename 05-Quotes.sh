eb38ea22a0204a9a07e1df560f67677dd0d3bc6f
manikmp <manivajrala1992@gmail.com>
2022-07-08 16:06:11 +0530

    variables , Arrays

---
+++ 05-Quotes.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo 'Apple cost is $100'
+echo "Apple cost is $200"
+echo "Apple cost is \$300"
+
+Training = Devops
+echo 'training = ${Training}'
+echo "training = ${Training}"
+echo "training = \${Training}"
