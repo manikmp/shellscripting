ce55db44ae9fd0171a0e4005498a4153d12378c8
manikmp <manivajrala1992@gmail.com>
2022-07-08 13:41:26 +0530

    variables , Arrays

--- 04-Inputs.sh
+++ 04-Inputs.sh
@@ -3,3 +3,16 @@
 
 read -p 'enter your name : ' name
 echo "your name = $name"
+
+#echo "Process id of shell = $$"
+#echo "Name of shell script = $0"
+#echo "Number of args = $#"
+#echo "Argument 1 = $1"
+#echo "Argument 2 = $2"
+
+echo "process id of shell  =$$"
+echo "Name of the shell script = $0"
+echo "Number of args = $#"
+echo "Arguments 1 = $1"
+echo "Arguments 2 = $2
+"
