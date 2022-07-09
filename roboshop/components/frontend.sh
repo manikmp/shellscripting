15a541908fe778bd2961dc6fb96fa81822678cd5
manikmp <manivajrala1992@gmail.com>
2022-07-09 10:40:06 +0530

    Makefile

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -6,6 +6,7 @@ yum install nginx -y
 echo -e "\e[36m Downloading nginx content \e[0m"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 
+echo -e "\e[36m cleanup old nginx content and extract now downloaded archive \e[0m"
 rm -rf /usr/share/nginx/html *
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
@@ -15,9 +16,10 @@ rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
 
-
+echo -e "\e[36m starting nginx \e[0m"
 systemctl restart nginx
 systemctl enable nginx
+
 
 
 
