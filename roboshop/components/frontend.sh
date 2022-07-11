75af1db6f7cdbc032d53df8c31dd0de00a5b6fa7
manikmp <manivajrala1992@gmail.com>
2022-07-11 20:33:56 +0530

    Makefile

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -5,11 +5,12 @@ statcheck() {
     echo -e "\e[32mSUCESS\e[0m"
   else
     echo -e "\e[31mfailure\e[0m"
-  exit 2
+  exit 1
   fi
 }
 
 print() {
+  echo -e "\n---------$1-------" &>>LOG_FILE
   echo -e "\e[36m $1 \e[0m"
 }
 
@@ -19,29 +20,31 @@ then
   echo -e "\e[32mSUCESS\e[om"
  else
    echo -e "\e[31mFAILURE\e[om"
-  exit 1
+  exit 2
 fi
 
+LOG_FILE=/tmp/roboshop.log
+rm -f $LOG_FILE
 print "Installing Nginx"
-yum install nginx -y
+yum install nginx -y >>LOG_FILE
 statcheck $?
-systemctl enable nginx
+systemctl enable nginx >>LOG_FILE
 statcheck $?
-systemctl start nginx
+systemctl start nginx >>LOG_FILE
 
 print "Downloading nginx content"
-curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >>LOG_FILE
 statcheck $?
 
 print "cleanup old Nginx content"
 cd /usr/share/nginx/html
-rm -rf *
+rm -rf * >>LOG_FILE
 print "Extracting Archive"
-unzip /tmp/frontend.zip && mv frontend-main/* .&& mv static/* .
+unzip /tmp/frontend.zip >>LOG_FILE && mv frontend-main/* . >>LOG_FILE && mv static/* . >>LOG_FILE
 rm -rf frontend-main README.md
 statcheck $?
 print "Update Roboshop configuration"
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
+mv localhost.conf /etc/nginx/default.d/roboshop.conf >>LOG_FILE
 statcheck $?
 
 print "starting nginx"
