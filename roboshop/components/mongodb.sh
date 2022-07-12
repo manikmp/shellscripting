509b00463fa9af1fd46fb5cda1e250346883c82f
manikmp <manivajrala1992@gmail.com>
2022-07-12 12:57:24 +0530

    Mongodb

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,26 +1,6 @@
 #!/usr/bin/env bash
 
-statcheck() {
-  if [ $1 -eq 0 ]; then
-    echo -e "\e[32mSUCCESS\e[0m"
-    else
-    echo -e "\e[31mFAILURE\e[0m"
-    exit 2
-  fi
-}
-
-print() {
-  echo -e "\n--------$1-------" &>>$LOG_FILE
-  echo -e "\e[36m $1 \e[0m"
-}
-
-USER_ID=$(id -u)
-   if [ "$USER_ID" -ne 0 ]; then
-     echo "you should run your script as sudo (or) root user"
-     exit 1
-   fi
-   LOG_FILE=/tmp/roboshop.log
-   rm -f $LOG_FILE
+source components/common.sh
 
 print "setup yum Repos"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG_FILE
@@ -36,5 +16,18 @@ statcheck $?
 
 print "start MONGODB"
 systemctl enable mongod &>>$LOG_FILE && systemctl restart mongod &>>$LOG_FILE
+statcheck $?
+
+print "Download Schema"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>LOG_FILE
+statcheck $?
+
+print "Extract Schema"
+cd /tmp && unzip mongodb.zip &>>$LOG_FILE
+statcheck $?
+
+print "Load Schema"
+cd mongodb-main && mongo < catalogue.js &>>$LOG_FILE
+mongo < users.js &>>$LOG_FILE
 statcheck $?
 
