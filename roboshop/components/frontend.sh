76d6c39f81d93a38190f69a3f2c15049022439d3
manikmp <manivajrala1992@gmail.com>
2022-07-12 10:08:47 +0530

    Mongodb

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,30 +1,7 @@
 #!/usr/bin/env bash
 
-statcheck() {
-  if [ $1 -eq 0 ]; then
-    echo -e "\e[32mSUCESS\e[0m"
-  else
-    echo -e "\e[31mfailure\e[0m"
-  exit 1
-  fi
-}
+source components/common.sh
 
-print() {
-  echo -e "\n---------$1-------" &>>$LOG_FILE
-  echo -e "\e[36m $1 \e[0m"
-}
-
-USER_ID=$(id -u)
-if [ "USER_ID -ne 0" ];
-then
-  echo -e "\e[32mSUCESS\e[om"
- else
-   echo -e "\e[31mFAILURE\e[om"
-  exit 2
-fi
-
-LOG_FILE=/tmp/roboshop.log
-rm -f $LOG_FILE
 print "Installing Nginx"
 yum install nginx -y >>LOG_FILE
 statcheck $?
