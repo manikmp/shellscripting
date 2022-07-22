69aa9bcf90f681704996301f3aacd18546f9f39a
manikmp <manivajrala1992@gmail.com>
2022-07-22 19:05:19 +0530

    catalogue component

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -37,3 +37,9 @@ print "Extract App Content"
  chown -R ${APP_USER}:${APP_USER}  /home/${APP_USER}
  statcheck $?
 
+ print "setup systemd file"
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/'
+/home/roboshop/catalogue/systemd.service &>>${LOG_FILE} &&  mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>${LOG_FILE}
+statcheck $?
+
+
