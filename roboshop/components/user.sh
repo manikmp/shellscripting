20f3a13e56673a692eedf0596b02aa3731e49cf2
manikmp <manivajrala1992@gmail.com>
2022-07-22 21:04:48 +0530

    user component

--- roboshop/components/user.sh
+++ roboshop/components/user.sh
@@ -28,3 +28,7 @@ statcheck $?
 print "Install app dependencies"
 cd /home/${APP_USER}/user &>>${LOG_FILE} && npm install &>>${LOG_FILE}
 statcheck $?
+
+print "Fix App User Permission"
+chown -R ${APP_USER}:${APP_USER} /home/${APP_USER}
+statcheck $?
