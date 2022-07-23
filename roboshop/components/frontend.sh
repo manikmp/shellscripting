16fdcdb080a460f8136245b833d0bf5385b99dd9
manikmp <manivajrala1992@gmail.com>
2022-07-23 10:17:34 +0530

    user component

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -23,6 +23,8 @@ statcheck $?
 print "Update Roboshop configuration"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf >>LOG_FILE
 sed -i -e '/catalogue/s/localhost/catalogue.roboshop.internal/' /etc/nginx/default.d/roboshop.conf &>>${LOG_FILE}
+sed -i -e '/user/s/localhost/user.roboshop.internal/' /etc/nginx/default.d/roboshop.conf &>>${LOG_FILE}
+sed -i -e '/cart/s/localhost/cart.roboshop.internal/' /etc/nginx/default.d/roboshop.conf &>>${LOG_FILE}
 statcheck $?
 
 print "starting nginx"
